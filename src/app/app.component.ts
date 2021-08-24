082cd34b767617345d3c1a3a54a7ff85ea2aa75c
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-24 09:11:39 +0300

    transfer the array from app component to quotes component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,7 @@ import { Quotes } from './quotes';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  quotes:Quotes[]=[
-    {id:1, name: 'perseverance is key'},
-    {id:2, name: 'slow steady and sure'},
-    {id:3, name: 'one foot infront of the other'}
-  ];
+export class AppComponent { 
 }
     
   
--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
-
+  quotes:Quotes[]=[
+    {id:1, name: 'perseverance is key'},
+    {id:2, name: 'slow steady and sure'},
+    {id:3, name: 'one foot infront of the other'}
+  ];
   constructor() { }
 
   ngOnInit(): void {
