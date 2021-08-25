724c1956deba7bbd463c60815f3a1a5ffc8375c2
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-08-25 12:01:55 +0300

    add strikethrough directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    QuotesDetailsComponent
+    QuotesDetailsComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule,
